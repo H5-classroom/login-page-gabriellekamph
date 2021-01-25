e115856213af0e034de948075a6673fe5b7d9b55
Gabrielle Kamph <gabrielle.kamph@gmail.com>
2021-01-25 14:11:29 +0100

    First draft with step-by-step pseudocode to solve problem

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// Array med användare och lösenord
+// SKAPA ARRAY MED ANVÄNDARUPPGIFTER
 
-// Meny 1: 
+// SÄTT EVENTLISTENER PÅ LOGGA IN-KNAPP
+
+// CHECKA OM IFYLLDA UPPGIFTER STÄMMER OCH UPPDATERA MENY + INNEHÅLL
+
+//----- Meny 1 (ej inloggad) - visa inloggningsformulär
+
+//----- Meny 2 (inloggad) - visa knapp för att logga ut
+
+//----- Innehållsvy 1 (ej inloggad) - välkomstsida för ej inloggade
+
+//----- Innehållsvy 2 (inloggad) - välkomstsida för inloggad besökare
+
+//----- Innehållsvy 3 (felaktig inloggning) - visa felmeddelande
