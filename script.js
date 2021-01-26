3b8359de38bbd884b06ef2066de9095383f92369
Gabrielle Kamph <gabrielle.kamph@gmail.com>
2021-01-26 23:51:44 +0100

    Making reload of page work correctly

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 let userArr = ["janne", "gabrielle"];
 let passArr = ["test", "mittlosen"];
 
-let username = document.getElementById("username").value;
-let passW = document.getElementById("passW").value;
+
 
 // KOLLA OM BESÖKAREN FINNS I LOCAL STORAGE
 
@@ -21,36 +20,41 @@ if (localStorage.getItem('username') !== null) {
 
 function welcomePage() {
 
-    let startpageContent = "<p> Det här är en simpel liten inloggningssida. <br /> Fyll i dina användaruppgifter ovan för att logga in. </p>";
+    document.getElementById("content").innerHTML = "<p> Det här är en simpel liten inloggningssida. <br /> Fyll i dina användaruppgifter ovan för att logga in. </p>";
     let loginNav = document.getElementById("topnav");
-
-    document.getElementById("content").insertAdjacentHTML("beforeend", startpageContent);
+    
 
     document.getElementById("loginBtn").addEventListener("click", validateInput);
+}
 
 // VALIDERA ANGIVNA INLOGGNINGSUPPGIFTER
 
-    function validateInput() {
-        for (let i=0; i <userArr.length; i++) {
-            if ((username == userArr[i]) && (passW == passArr[i])) {
-                localStorage.setItem("username", userArr[i]);
-                valid = true;
-                loggedIn();
-                }
-        
-            else {
-                errorMessage();
+function validateInput() {
+
+    let username = document.getElementById("username").value;
+    let passW = document.getElementById("passW").value;
+
+    for (let i=0; i <userArr.length; i++) {
+        if ((username == userArr[i]) && (passW == passArr[i])) {
+            localStorage.setItem("username", userArr[i]);
+            console.log("yay");
+            loggedIn();
             }
+    
+        else {
+            errorMessage();
         }
     }
-
 }
 
 // STARTSIDA FÖR INLOGGAT LÄGE
 
 function loggedIn() {
 
-    document.getElementById("content").innerHTML = "<h2>Snyggt " + username + "!</h2> <p>Nu är du inloggad.</p>";
+    document.getElementById("topnav").innerHTML = "<button id='logoutBtn'> Logga ut </button>";
+    document.getElementById("content").innerHTML = "<h2>Snyggt " + localStorage.getItem('username') + "!</h2> <p>Nu är du inloggad.</p>";
+
+    document.getElementById("logoutBtn").addEventListener("click", welcomePage);
 
 }
 
